0ad26af73c2900af2d82895e581e50c9a97beceb
vsvetlov <vsvetlov@gmail.com>
2021-03-08 19:28:54 +0300

    Simplify game-progression functions

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -12,35 +12,28 @@ def start_game():
     play_game(GAME_RULES, MAX_ATTEMPTS, make_attempt_progression)
 
 
-def create_progression(game_complexity):  # noqa: WPS210
+def create_progression(game_complexity):
     """
     Create random progressions with defined complexity.
 
-    Return progression, missed number
+    Return progression, missed position
 
     Args:
         game_complexity: first number, diff, length of progression
 
     Returns:
-        str, int
+        tuple, int
     """
     first_number = random.randrange(game_complexity[0])
     diff = random.randrange(2, game_complexity[1])
     progression_length = random.randrange(6, game_complexity[2])
-    missed_position = random.randrange(1, progression_length + 1)
-    current_position = 1
-    progression = ''
+    missed_position = random.randrange(progression_length)
+    progression = ()
     current_number = first_number
-    missed_number = 0
-    while current_position <= progression_length:
-        if missed_position == current_position:
-            progression += ' ..'
-            missed_number = current_number
-        else:
-            progression += ' ' + str(current_number)
+    for _ in range(progression_length):
+        progression += (str(current_number),)
         current_number += diff
-        current_position += 1
-    return progression, missed_number
+    return progression, missed_position
 
 
 def make_attempt_progression():
@@ -53,6 +46,11 @@ def make_attempt_progression():
         str
     """
     game_complexity = (11, 5, 11)
-    progression, correct_answer = create_progression(game_complexity)
-    game_question = 'Question:{0}'.format(progression)
+    progression, missed_position = create_progression(game_complexity)
+    correct_answer = progression[missed_position]
+    game_question = 'Question: {0}{1}{2}'.format(
+        ' '.join(progression[:missed_position]),
+        ' .. ',
+        ' '.join(progression[missed_position + 1:]),
+    )
     return game_question, str(correct_answer)
